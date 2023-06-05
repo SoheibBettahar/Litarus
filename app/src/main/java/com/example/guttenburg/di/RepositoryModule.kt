7af5bbb8bc5cb10175cef414a1f9b7fc85bb6400
SoheibBettahar <soheibbett@gmail.com>
2023-06-05 11:42:07 +0100

    Add RepositoryModule.kt, DispatcherModule.kt

--- app/src/main/java/com/example/guttenburg/di/RepositoryModule.kt
+++ app/src/main/java/com/example/guttenburg/di/RepositoryModule.kt
@@ -1,35 +1,18 @@
 package com.example.guttenburg.di
 
-import com.example.guttenburg.data.database.GuttenburgDatabase
-import com.example.guttenburg.data.database.LocalDataSource
-import com.example.guttenburg.data.network.RemoteDataSource
 import com.example.guttenburg.data.repository.BooksRepository
 import com.example.guttenburg.data.repository.BooksRepositoryImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-class RepositoryModule {
-
-    @Provides
-    fun provideIODispatcher(): CoroutineDispatcher {
-        return Dispatchers.IO
-    }
-
+abstract class RepositoryModule {
     @Singleton
-    @Provides
-    fun bindBookRepository(
-        remoteDataSource: RemoteDataSource,
-        localDataSource: LocalDataSource,
-        ioDispatcher: CoroutineDispatcher
-    ): BooksRepository {
-        return BooksRepositoryImpl(remoteDataSource, localDataSource, ioDispatcher)
-    }
+    @Binds
+    abstract fun bindBookRepository(repository: BooksRepositoryImpl): BooksRepository
 
 }
