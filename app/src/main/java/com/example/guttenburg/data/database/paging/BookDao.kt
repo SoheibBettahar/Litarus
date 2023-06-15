8561eacca535c7ff2a10024c8fc8530214c22638
SoheibBettahar <soheibbett@gmail.com>
2023-06-15 13:00:10 +0100

    Add get functions to BookDao

--- app/src/main/java/com/example/guttenburg/data/database/paging/BookDao.kt
+++ app/src/main/java/com/example/guttenburg/data/database/paging/BookDao.kt
@@ -5,18 +5,34 @@ import androidx.room.*
 
 @Dao
 interface BookDao {
+    @Query("SELECT * FROM book WHERE (title LIKE :searchText OR authors LIKE :searchText) " +
+            "AND subjects LIKE :category AND languages LIKE :languages ORDER BY page")
+    fun getBySearchAndCategoryAndLanguages(searchText: String, category: String, languages: String): PagingSource<Int, DatabaseBook>
+
+    @Query("SELECT * FROM book WHERE (title LIKE :searchText OR authors LIKE :searchText) " +
+            "AND subjects LIKE :category ORDER BY page")
+    fun getBySearchAndCategory(searchText: String, category: String): PagingSource<Int, DatabaseBook>
+
+    @Query("SELECT * FROM book WHERE (title LIKE :searchText OR authors LIKE :searchText) " +
+            "AND languages LIKE :languages ORDER BY page")
+    fun getBySearchAndLanguages(searchText: String, languages: String): PagingSource<Int, DatabaseBook>
+
+    @Query("SELECT * FROM book WHERE subjects LIKE :category " +
+            "AND languages LIKE :languages ORDER BY page")
+    fun getByCategoryAndLanguages(category: String, languages: String): PagingSource<Int, DatabaseBook>
+
+    @Query("SELECT * FROM book WHERE (title LIKE :searchText OR authors LIKE :searchText) " +
+            "ORDER BY page")
+    fun getBySearch(searchText: String): PagingSource<Int, DatabaseBook>
+
+    @Query("SELECT * FROM book WHERE subjects LIKE :category ORDER BY page" )
+    fun getByCategory(category: String): PagingSource<Int, DatabaseBook>
+
+    @Query("SELECT * FROM book WHERE languages LIKE :languages ORDER BY page")
+    fun getByLanguages(languages: String): PagingSource<Int, DatabaseBook>
 
     @Query("SELECT * FROM book ORDER BY page")
     fun getAll(): PagingSource<Int, DatabaseBook>
-
-    @Query(
-        "SELECT * FROM book WHERE (title LIKE :searchText OR authors LIKE :searchText) " +
-                "AND subjects LIKE :category ORDER BY page"
-    )
-    fun booksByNameOrAuthorAndCategory(
-        searchText: String,
-        category: String
-    ): PagingSource<Int, DatabaseBook>
 
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
