4ef1769edea691f34158cc0b96ed7136dab25ee2
SoheibBettahar <soheibbett@gmail.com>
2023-06-18 18:04:57 +0100

    Clean ComposeTraining.kt

--- app/src/main/java/com/example/guttenburg/ui/screens/ComposeTraining.kt
+++ app/src/main/java/com/example/guttenburg/ui/screens/ComposeTraining.kt
@@ -1,26 +1,16 @@
 package com.example.guttenburg.ui.screens
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.LoadState
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.example.guttenburg.ui.viewmodels.BooksViewModel
+import com.example.guttenburg.ui.viewmodels.TrainingViewModel
 
 
 private const val TAG = "TrainingScreen"
 
 @Composable
-fun TrainingScreen(viewModel: BooksViewModel = hiltViewModel()) {
+fun TrainingScreen(viewModel: TrainingViewModel = hiltViewModel()) {
 }
+
+
 
 
