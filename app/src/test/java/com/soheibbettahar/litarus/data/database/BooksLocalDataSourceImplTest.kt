446cb107e938ce8d4b286dd761f02ade3a2b64f3
SoheibBettahar <soheibbett@gmail.com>
2023-09-01 11:39:23 +0100

    Create empty test classes for BookLocalDataSourceImpl and BookViewModel

---
+++ app/src/test/java/com/soheibbettahar/litarus/data/database/BooksLocalDataSourceImplTest.kt
@@ -0,0 +1,16 @@
+package com.soheibbettahar.litarus.data.database
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+
+
+internal class BooksLocalDataSourceImplTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+}
---
+++ app/src/test/java/com/soheibbettahar/litarus/ui/viewmodels/BooksViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.soheibbettahar.litarus.ui.viewmodels
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class BooksViewModelTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun getSearchTerm() {
+    }
+
+    @Test
+    fun getCategory() {
+    }
+
+    @Test
+    fun getLanguage() {
+    }
+
+    @Test
+    fun getSearchedBooksPagingDataFlow() {
+    }
+
+    @Test
+    fun updateCategory() {
+    }
+
+    @Test
+    fun updateSearchTerm() {
+    }
+
+    @Test
+    fun updateLanguage() {
+    }
+}
